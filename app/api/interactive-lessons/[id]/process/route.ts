e44ae6ab0ea0fef2c797f282634d8458f2ea9bb6
aahadr1 <aahadri55@gmail.com>
2025-11-30 00:17:05 +0200

    fix: Add DOMMatrix and Path2D polyfills for pdf-parse in Node.js
    - pdf-parse/pdfjs-dist requires browser globals
    - Added minimal polyfills for DOMMatrix and Path2D
    - Fixes 'DOMMatrix is not defined' error during PDF processing

--- app/api/interactive-lessons/[id]/process/route.ts
+++ app/api/interactive-lessons/[id]/process/route.ts
@@ -82,6 +82,51 @@ interface Question {
   explanation: string
 }
 
+// Polyfill browser globals for Node.js (required by pdf-parse/pdfjs-dist)
+if (typeof globalThis.DOMMatrix === 'undefined') {
+  // @ts-ignore - Minimal DOMMatrix polyfill
+  globalThis.DOMMatrix = class DOMMatrix {
+    m11 = 1; m12 = 0; m13 = 0; m14 = 0
+    m21 = 0; m22 = 1; m23 = 0; m24 = 0
+    m31 = 0; m32 = 0; m33 = 1; m34 = 0
+    m41 = 0; m42 = 0; m43 = 0; m44 = 1
+    a = 1; b = 0; c = 0; d = 1; e = 0; f = 0
+    is2D = true; isIdentity = true
+    
+    constructor(init?: string | number[]) {
+      if (Array.isArray(init) && init.length === 6) {
+        [this.a, this.b, this.c, this.d, this.e, this.f] = init
+        this.m11 = this.a; this.m12 = this.b
+        this.m21 = this.c; this.m22 = this.d
+        this.m41 = this.e; this.m42 = this.f
+      }
+    }
+    
+    inverse() { return new DOMMatrix() }
+    multiply() { return new DOMMatrix() }
+    translate() { return new DOMMatrix() }
+    scale() { return new DOMMatrix() }
+    rotate() { return new DOMMatrix() }
+    transformPoint(p: any) { return p }
+  }
+}
+
+if (typeof globalThis.Path2D === 'undefined') {
+  // @ts-ignore - Minimal Path2D polyfill
+  globalThis.Path2D = class Path2D {
+    constructor() {}
+    addPath() {}
+    closePath() {}
+    moveTo() {}
+    lineTo() {}
+    bezierCurveTo() {}
+    quadraticCurveTo() {}
+    arc() {}
+    arcTo() {}
+    rect() {}
+  }
+}
+
 // Extract text from PDF using pdf-parse
 async function extractPdfText(buffer: Buffer): Promise<string[]> {
   // Use require for pdf-parse as it's a CommonJS module
